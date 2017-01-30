ba8078eb3eb8e819c1028bad82416e92576de0c3
Simmy <simmycop@gmail.com>
2017-01-29 22:36:03 -0500

    Update adi(3).py

--- adi(3).py
+++ adi(3).py
@@ -1,3 +1,62 @@
+from __future__ import absolute_import, division, print_function
+import requests, time, random, string, os
+from bs4 import BeautifulSoup
+from GmailDotEmailGenerator import GmailDotEmailGenerator
+
+
+# TODO : ADD A LIMIT To AMOUNT OF ACCS TO BE CREATED!
+
+def account_successfully_createdUS(response):
+    try:
+        return False if BeautifulSoup(response.text, "html.parser").find('input',
+                                                                         {'id': 'resumeURL'}).get('value') == \
+                        'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/MyAccount-CreateOrLogin' \
+            else True
+    except:
+        return True
+
+
+def US():
+    print('WE ARE GENERATING FOR ADIDAS US')
+
+    basemail = input('Enter prefix of your email\t')
+
+    randompass = input('Do you want a random pass? Y for Yes. Any other Key for No\t')
+    if randompass == 'y' and 'y':
+        print('Generating Random passwords.')
+    else:
+        password = input('Enter Desired Password\t')
+    accountstogen = input('Enter Desired Accounts to be Made\t')
+    accountstogen = int(accountstogen)
+
+    for email in \
+            (GmailDotEmailGenerator(basemail + '@gmail.com').generate())[:accountstogen]:
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36',
+            'Accept-Encoding': 'gzip, deflate, sdch, br',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Upgrade-Insecure-Requests': '1'
+        }
+        if randompass == 'y' and 'y':
+            length = 13
+            chars = string.ascii_letters + string.digits + '$&@?!#%'
+            random.seed = (os.urandom(1024))
+            password = ''.join(random.choice(chars) for i in range(length))
+
+        s = requests.Session()
+        s.headers.update(headers)
+
+        r = s.get('https://cp.adidas.com/web/eCom/en_US/loadcreateaccount')
+        csrftoken = BeautifulSoup(r.text, "html.parser").find('input', {'name': 'CSRFToken'}).get('value')
+
+        s.headers.update({
+            'Origin': 'https://cp.adidas.com',
+            'Referer': 'https://cp.adidas.com/web/eCom/en_US/loadcreateaccount',
+        })
+        r = s.post('https://cp.adidas.com/web/eCom/en_US/accountcreate',
+                   data={
+                       'firstName': 'YOUR_FIRST_NAME',  ### Set your Name
                        'lastName': 'Sarmiento',  # Set your name
                        'minAgeCheck': 'true',
                        '_minAgeCheck': 'on',
